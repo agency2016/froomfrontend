a1f865bb05a5351dce3456a578abcdb06a31509d
Yasir Taher <ad-bd01@Ad-bd01s-Mac-mini.local>
2015-09-01 15:29:35 +0600

    froomfrontend commit 1
    navbar, header and footer

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 <html lang="en">
 
 <head>
-
-
-
     <title>froom</title>
 
     <!-- Bootstrap Core CSS -->
@@ -51,14 +48,17 @@
         <!-- /.container -->
     </nav>
 
+
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
-    <h1>Something different</h1>
+        <h1>Something different</h1>
     </header>
+<!-- /. Header Carousel -->
 
     <!-- Page Content -->
     <div class="container">
         <hr>
+        <!-- row -->
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
@@ -66,13 +66,13 @@
                 </h1>
             </div>
         </div>
-
+        <!-- /.row -->
 
         <!-- Footer -->
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; 2015</p>
                 </div>
             </div>
         </footer>
@@ -85,13 +85,6 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Script to Activate the Carousel -->
-    <script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-    </script>
 
 </body>
 
