6e35f0b00b1c94a410e67fc3357628e3d1e9d69c
Yasir Taher <ad-bd01@Ad-bd01s-Mac-mini.local>
2015-09-01 16:11:41 +0600

    slider added

--- index.php
+++ index.php
@@ -33,16 +33,15 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
                         <a href="#">About</a>
                     </li>
                     <li>
                         <a href="#">Portfolio</a>
                     </li>
-                    <li>
-                        <a href="#">Portfolio2<li>
-                        <a href="#">Portfolio</a>
-                    </li></a>
-                    </li>
+
                     <li>
                         <a href="#">Services</a>
                     </li>
@@ -59,9 +58,44 @@
 
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
-        <h1>Something different</h1>
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
     </header>
-<!-- /. Header Carousel -->
+    <!-- /. Header Carousel -->
 
     <!-- Page Content -->
     <div class="container">
