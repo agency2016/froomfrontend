15710f99cd3f813880745ba92cff139e02f9f736
Yasir Taher <ad-bd01@Ad-bd01s-Mac-mini.local>
2015-09-02 15:35:46 +0600

    search bar

--- index.php
+++ index.php
@@ -16,8 +16,65 @@
 
 <body>
 
+<!-- 1st navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">F-ROOM</a>
+        </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+            <li><p class="navbar-text">Already have an account?</p></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+                <ul id="login-dp" class="dropdown-menu">
+                    <li>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                        <div class="help-block text-right">
+                                            <a href="">Forget the password ?</a>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> keep me logged-in
+                                        </label>
+                                        </div>
+                                </form>
+                            </div>
+                            <div class="bottom text-center">
+                                New here ? <a href="#"><b>Join Us</b></a>
+                            </div>
+                     </div>
+                </li>
+            </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
     <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-default navbar-static-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -27,7 +84,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">F-ROOM</a>
+                <!-- <a class="navbar-brand" href="#">F-ROOM</a> -->
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -87,16 +144,50 @@
             </div>
         </div>
 
-        <form class="col-sm-12" id="searchForm">
-            <div class="form-group col-sm-4 col-sm-offset-4">
+<!--        <form class="col-sm-12" id="searchForm">
+           <div class="row row-centered">
+            <div class="form-group col-sm-6 col-centered">
                 <div class="input-group">
+                    
                     <input type="text" class="form-control" placeholder="Search for...">
                     <span class="input-group-btn">
                         <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-search"></i></button>
                     </span>
                 </div>
             </div>
-        </form>
+            </div>
+        </form> 
+ -->
+             <form class="col-sm-12 form-inline" id="searchForm">
+            <div class="form-group col-sm-6 col-sm-offset-4">
+            <select id="selectUser" style="width:auto;" class="form-control col-md-4">
+                        <option class="">One</option>
+                        <option class="">Two</option>
+                        <option class="">Three</option>
+             </select>
+              <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-search"></i></button>
+                    </span>
+                </div>
+            </div>
+        </form> 
+
+
+<!--         <form role="form" class="form-inline col-sm-12" id="searchForm">  
+            <div class="form-group col-sm-4 col-sm-offset-4">
+                <select id="selectUser" style="width:auto;" class="form-control selectWidth">
+                    <option class="">One</option>
+                    <option class="">Two</option>
+                    <option class="">Three</option>
+                </select>
+      <div class="btn-group">
+        <input type="text" class="form-control" placeholder="Search for...">
+      </div>
+        <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-search"></i></button>
+      </div>
+      </form> -->
 
         <!-- Controls -->
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
