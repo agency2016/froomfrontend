b5bae981a8097df1bb5a24d9b1de0d645bf67212
Yasir Taher <ad-bd01@Ad-bd01s-Mac-mini.local>
2015-09-06 10:50:41 +0600

    navbar account search bar

--- index.php
+++ index.php
@@ -20,8 +20,9 @@
 
 <body>
 
+
 <!-- 1st navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top navbar-1" role="navigation">
+<!-- <nav class="navbar navbar-inverse navbar-fixed-top navbar-1" role="navigation">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
@@ -72,13 +73,77 @@
             </ul>
         </li>
       </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+    </div>
+  </div>
+</nav> -->
 
+<!-- HEADER -->
+<header class="navbar-fixed-top" role="banner">
+
+<div id="topupmenu">
+  <div class="container">
+    <div class="row">
+       <div class="col col-xs-12 col-sm-6"></div>
+           <div style="text-align: right;" class="col col-xs-12 col-sm-6 account">
+      <!-- <div class="collapse navbar-collapse" id="navbar-collapse-1"> -->
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Account</b> <span class="caret"></span></a>
+                <ul id="login-dp" class="dropdown-menu">
+                    <li>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                        <div class="help-block text-right">
+                                            <a href="">Forget the password ?</a>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-warning btn-block">Sign in</button>
+                                    </div>
+                                    <div class="checkbox" id="cbox">
+                                        <label>
+                                            <input type="checkbox"> keep me logged-in
+                                        </label>
+                                        </div>
+                                </form>
+                            </div>
+                            <div class="bottom text-center">
+                                New here ? <a href="#"><b>Join Us</b></a>
+                            </div>
+                     </div>
+                </li>
+            </ul>
+        </li>
+      </ul>
+    <!-- </div> -->
+    </div>
+    </div>
+  </div>
+</div>
+
+<div id="brandlook">
+<div class="container">
+  <div class="row">
+    <div class="col col-xs-12 col-sm-6 logo">
+      <h1>F-ROOM</h1>
+    </div>
+    <div style="text-align: right;" class="col col-xs-12 col-sm-6">
+
+    </div>
+  </div>
+</div> 
+</div>
 
 <!-- Navigation -->
-<nav class="navbar navbar-default navbar-static-top navbar-2" role="navigation">
+<div class="navbar navbar-default " role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
@@ -114,11 +179,13 @@
         <!-- /.navbar-collapse -->
     </div>
     <!-- /.container -->
-</nav>
+</div>
 
-
+</header>
 <!-- Header Carousel -->
-<header id="myCarousel" class="carousel slide">
+
+<!-- Carousel Slider -->
+<div id="myCarousel" class="carousel slide">
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -148,8 +215,7 @@
         </div>
     </div>
 
-<!-- search n filter bar -->
-                                   
+<!-- search n filter bar -->                                  
 <div class="container" id="searchForm">
     <div class="row centered">    
         
@@ -262,10 +328,7 @@
     </div>
 </div>
 
-
-
 <!-- /. search n filter bar -->
-
 
 <!-- Controls -->
 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
@@ -274,10 +337,11 @@
 <a class="right carousel-control" href="#myCarousel" data-slide="next">
     <span class="icon-next"></span>
 </a>
-</header>
-    <!-- /. Header Carousel -->
+</div>    
+<!-- /. Carousel Slider-->
 
-    <!-- Page Content -->
+
+<!-- Page Content -->
 <div class="container">
     <hr>
     <!-- row -->
@@ -326,21 +390,8 @@
 <!-- /.container -->
 <!-- /.Page Content -->
 
-<!-- Footer -->
-<!-- <footer>
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 copy">
-            <p>Copyright &copy; Agency Delta 2015</p>
-        </div>
-        <div class="col-md-6 social-icons">
-            <a class="social-roll" href="#"><img src="img/facebook.png" alt="facebook.png"></a>
-            <a class="social-roll" href="#"><img src="img/twitter.png" alt="twitter.png"></a>
-        </div>
-    </div>
-    </div>
-</footer> -->
-  <div class="footer-bottom navbar navbar-default navbar-fixed-bottom">
+
+<footer id="footer-bottom">
         <div class="container">
           <div class="row">
             <div class="col-md-6 copy"><p>&copy; 2015 All rights reserved</p></div>
@@ -354,7 +405,7 @@
             </div>
         </div>
    </div>
-</div>
+</footer>
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
