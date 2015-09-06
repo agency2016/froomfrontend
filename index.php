5f9ed72188605b41ba99a2783b72006788497dd3
Yasir Taher <ad-bd01@Ad-bd01s-Mac-mini.local>
2015-09-06 12:50:48 +0600

    social icons

--- index.php
+++ index.php
@@ -20,114 +20,8 @@
 
 <body>
 
-
-<!-- 1st navigation -->
-<!-- <nav class="navbar navbar-inverse navbar-fixed-top navbar-1" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">F-ROOM</a>
-        </div>
-
-    <div class="collapse navbar-collapse" id="navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-right">
-            <li><p class="navbar-text">Already have an account?</p></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-                <ul id="login-dp" class="dropdown-menu">
-                    <li>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                    <div class="form-group">
-                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                        <div class="help-block text-right">
-                                            <a href="">Forget the password ?</a>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-warning btn-block">Sign in</button>
-                                    </div>
-                                    <div class="checkbox" id="cbox">
-                                        <label>
-                                            <input type="checkbox"> keep me logged-in
-                                        </label>
-                                        </div>
-                                </form>
-                            </div>
-                            <div class="bottom text-center">
-                                New here ? <a href="#"><b>Join Us</b></a>
-                            </div>
-                     </div>
-                </li>
-            </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav> -->
-
 <!-- HEADER -->
 <header class="navbar-fixed-top" role="banner">
-
-<div id="topupmenu">
-  <div class="container">
-    <div class="row">
-       <div class="col col-xs-12 col-sm-6"></div>
-           <div style="text-align: right;" class="col col-xs-12 col-sm-6 account">
-      <!-- <div class="collapse navbar-collapse" id="navbar-collapse-1"> -->
-        <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Account</b> <span class="caret"></span></a>
-                <ul id="login-dp" class="dropdown-menu">
-                    <li>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                    <div class="form-group">
-                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                        <div class="help-block text-right">
-                                            <a href="">Forget the password ?</a>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-warning btn-block">Sign in</button>
-                                    </div>
-                                    <div class="checkbox" id="cbox">
-                                        <label>
-                                            <input type="checkbox"> keep me logged-in
-                                        </label>
-                                        </div>
-                                </form>
-                            </div>
-                            <div class="bottom text-center">
-                                New here ? <a href="#"><b>Join Us</b></a>
-                            </div>
-                     </div>
-                </li>
-            </ul>
-        </li>
-      </ul>
-    <!-- </div> -->
-    </div>
-    </div>
-  </div>
-</div>
 
 <div id="brandlook">
 <div class="container">
@@ -136,10 +30,10 @@
       <h1>F-ROOM</h1>
     </div>
     <div style="text-align: right;" class="col col-xs-12 col-sm-6">
-
+        
     </div>
-  </div>
-</div> 
+  </div> 
+</div>
 </div>
 
 <!-- Navigation -->
@@ -174,6 +68,43 @@
                 <li>
                     <a href="#">Contact</a>
                 </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Account</b> <span class="caret"></span></a>
+                  <ul id="login-dp" class="dropdown-menu">
+                    <li>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                        <div class="help-block text-right">
+                                            <a href="">Forget the password ?</a>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-warning btn-block">Sign in</button>
+                                    </div>
+                                    <div class="checkbox" id="cbox">
+                                        <label>
+                                            <input type="checkbox"> keep me logged-in
+                                        </label>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="bottom text-center">
+                                New here ? <a href="#"><b>Join Us</b></a>
+                            </div>
+                          </div>
+                      </li>
+                  </ul>
+                </li>
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
@@ -196,19 +127,19 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
         <div class="item active">
-            <div class="fill" style="background-image:url('img/flash.jpg');"></div>
+            <div class="fill" style="background-image:url('img/img1.jpg');"></div>
             <!-- <div class="carousel-caption">
                 <h2>Caption 1</h2>
             </div> -->
         </div>
         <div class="item">
-            <div class="fill" style="background-image:url('img/superman.jpg');"></div>
+            <div class="fill" style="background-image:url('img/img2.jpg');"></div>
                 <!-- <div class="carousel-caption">
                     <h2>Caption 2</h2>
                 </div> -->
         </div>
         <div class="item">
-            <div class="fill" style="background-image:url('img/gl.png');"></div>
+            <div class="fill" style="background-image:url('img/img3.jpg');"></div>
                 <!-- <div class="carousel-caption">
                     <h2>Caption 3</h2>
                 </div> -->
@@ -391,22 +322,24 @@
 <!-- /.Page Content -->
 
 
+
+<!-- FOOTER -->
 <footer id="footer-bottom">
         <div class="container">
           <div class="row">
-            <div class="col-md-6 copy"><p>&copy; 2015 All rights reserved</p></div>
-            <div class="col-md-6 social-icons">
-                <a class="social-roll" href="#"><img src="img/facebook.png" alt="facebook.png"></a>
-                <a class="social-roll" href="#"><img src="img/twitter.png" alt="twitter.png"></a>
-                <a class="social-roll" href="#"><img src="img/google+.png" alt="google+.png"></a>
-                <a class="social-roll" href="#"><img src="img/instagram.png" alt="instagram.png"></a>
-                <a class="social-roll" href="#"><img src="img/pinterest.png" alt="pinterest.png"></a>
-
+            <div class="col col-md-6 col-xs-6 copy"><p>&copy; 2015 All rights reserved</p></div>
+            <div class="col col-md-6 col-xs-6 social-icons">
+              <div class="row">
+                 <a href=""> <i data-toggle="tooltip" data-placement="top" title="Facebook" class="social-roll fa fa-facebook"></i></a>
+                 <a href=""> <i data-toggle="tooltip" data-placement="top" title="Twitter" class="social-roll fa fa-twitter"></i></a>
+                 <a href=""> <i data-toggle="tooltip" data-placement="top" title="Google Plus" class="social-roll fa fa-google-plus"></i></a>
+                 <a href=""> <i data-toggle="tooltip" data-placement="top" title="Linkedin" class="social-roll fa fa-linkedin"></i></a>
+              </div>
             </div>
         </div>
    </div>
 </footer>
-
+<!-- /. FOOTER -->
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
@@ -470,6 +403,11 @@ $(document).ready(function(e){
         $('.input-group #search_param').val(param);
     });
 });
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
 </script>
 
