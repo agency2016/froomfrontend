49b9d5546afd9eb6d1e3bca4c1c07b4f5dc02a88
maria <email.rothy@gmail.com>
2015-10-14 14:04:45 +0600

    db back up

---
+++ editor.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+<script
+    src="js/trumbowyg.js">
+</script>
+
+<link href="css/trumbowyg.css" rel="stylesheet" type="text/css">
+ 
+</head>
+
+<body>
+    <textarea id="trumbowyg-demo"></textarea>
+
+</body>
+</html>
